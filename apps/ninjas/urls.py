f40702e79b6fd6bb2592080030cac42d4d1e617c
Troy Center <troycenter1@gmail.com>
2017-06-20 15:32:52 -0700

    ninjas are all done.l Need to fix the error page and add the lady to it.

--- apps/ninjas/urls.py
+++ apps/ninjas/urls.py
@@ -2,4 +2,10 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^ninjas$', views.ninjas),
+    url(r'^ninjas/blue$', views.blue),
+    url(r'^ninjas/orange$', views.orange),
+    url(r'^ninjas/red$', views.red),
+    url(r'^ninjas/purple$', views.purple),
+    url(r'^ninjas/(?P<hackerurl>)', views.invalid),
 ]
--- apps/ninjas/views.py
+++ apps/ninjas/views.py
@@ -4,5 +4,24 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-def home(request): 
+def home(request):
+    '''Render the index home page with no ninjas'''
     return render(request, 'ninjas/index.html')
+def ninjas(request):
+    '''Render the nindas page, with buttons to select a ninja'''
+    return render(request, 'ninjas/ninjas.html')
+def blue(request):
+    '''render the blue ninja, Leonardo'''
+    return render(request, 'ninjas/blue.html')
+def orange(request):
+    '''render the orange ninja, Machelangelo'''
+    return render(request, 'ninjas/orange.html')
+def red(request):
+    '''render the red ninja, Raphael'''
+    return render(request, 'ninjas/red.html')
+def purple(request):
+    '''render the purple ninja, Donatello'''
+    return render(request, 'ninjas/purple.html')
+def invalid(request):
+    ''' render the invalid page with April Oneil on it'''
+    return render(request, 'ninjas/invalid.html')
